ab3ff3e433a7cd5147034ede11599db6280ea467
sjsdfg <736777445@qq.com>
2019-04-26 10:45:58 +0800

    add modules

---
+++ hiding/src/main/java/mypackage/MyClass.java
@@ -0,0 +1,9 @@
+// hiding/mypackage/MyClass.java
+// (c)2017 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+package mypackage;
+
+public class MyClass {
+  // ...
+}
