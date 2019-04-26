e034e14113f0cb5aa1dee88dc9882bb27e121b41
sjsdfg <736777445@qq.com>
2019-04-26 10:40:18 +0800

    add modules

---
+++ generics/src/main/java/MultipleInterfaceVariants.java
@@ -0,0 +1,12 @@
+// generics/MultipleInterfaceVariants.java
+// (c)2017 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+// {WillNotCompile}
+
+interface Payable<T> {}
+
+class Employee implements Payable<Employee> {}
+
+class Hourly extends Employee
+implements Payable<Hourly> {}
---
+++ generics/src/main/java/coffee/Breve.java
@@ -0,0 +1,6 @@
+// generics/coffee/Breve.java
+// (c)2017 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+package coffee;
+public class Breve extends Coffee {}
